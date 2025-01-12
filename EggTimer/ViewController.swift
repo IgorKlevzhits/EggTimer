4019fc0f74e1446a28e3d62c6fc8295661ca0bb9
IgorKlevzhits <klevzhits94@icloud.com>
2025-01-12 05:47:36 +0300

    Create UI and add alarm

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -8,12 +8,171 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - UI
+    
+    private lazy var mainStackView: UIStackView = {
+        let element = UIStackView()
+        element.spacing = 0
+        element.distribution = .fillEqually
+        element.axis = .vertical
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let element = UILabel()
+        element.text = "How do you like your eggs?"
+        element.font = .systemFont(ofSize: 25)
+        element.textAlignment = .center
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var eggsStackView: UIStackView = {
+        let element = UIStackView()
+        element.axis = .horizontal
+        element.distribution = .fillEqually
+        element.spacing = 20
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var softImageView: UIImageView = {
+        let element = UIImageView()
+        element.image = UIImage(named: "soft_egg")
+        element.contentMode = .scaleAspectFit
+        element.isUserInteractionEnabled = true
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var softButton: UIButton = {
+        let element = UIButton(type: .system)
+        element.setTitle("Soft", for: .normal)
+        element.titleLabel?.font = .systemFont(ofSize: 18, weight: .black)
+        element.tintColor = .white
+        element.addTarget(self, action: #selector(eggsButtonsTapped), for: .touchUpInside)
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var mediumImageView: UIImageView = {
+        let element = UIImageView()
+        element.image = UIImage(named: "medium_egg")
+        element.contentMode = .scaleAspectFit
+        element.isUserInteractionEnabled = true
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var mediumButton: UIButton = {
+        let element = UIButton(type: .system)
+        element.setTitle("Medium", for: .normal)
+        element.titleLabel?.font = .systemFont(ofSize: 18, weight: .black)
+        element.tintColor = .white
+        element.addTarget(self, action: #selector(eggsButtonsTapped), for: .touchUpInside)
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var hardImageView: UIImageView = {
+        let element = UIImageView()
+        element.image = UIImage(named: "hard_egg")
+        element.contentMode = .scaleAspectFit
+        element.isUserInteractionEnabled = true
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var hardButton: UIButton = {
+        let element = UIButton(type: .system)
+        element.setTitle("Hard", for: .normal)
+        element.titleLabel?.font = .systemFont(ofSize: 18, weight: .black)
+        element.tintColor = .white
+        element.addTarget(self, action: #selector(eggsButtonsTapped), for: .touchUpInside)
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var timerView: UIView = {
+        let element = UIView()
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var progressView: UIProgressView = {
+        let element = UIProgressView()
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    // MARK: - Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setViews()
+        setupConstraints()
     }
-
+    
+    @objc private func eggsButtonsTapped(_ sender: UIButton) {
+        print(sender.currentTitle)
+    }
 
 }
 
+// MARK: - Set Views ans Set Constraints
+
+extension ViewController {
+    
+    private func setViews() {
+        view.backgroundColor = .systemCyan
+        view.addSubview(mainStackView)
+        
+        mainStackView.addArrangedSubview(titleLabel)
+        mainStackView.addArrangedSubview(eggsStackView)
+        mainStackView.addArrangedSubview(timerView)
+        
+        eggsStackView.addArrangedSubview(softImageView)
+        eggsStackView.addArrangedSubview(mediumImageView)
+        eggsStackView.addArrangedSubview(hardImageView)
+        
+        softImageView.addSubview(softButton)
+        mediumImageView.addSubview(mediumButton)
+        hardImageView.addSubview(hardButton)
+        
+        timerView.addSubview(progressView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mainStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            mainStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            mainStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            
+            progressView.centerYAnchor.constraint(equalTo: timerView.centerYAnchor),
+            progressView.leadingAnchor.constraint(equalTo: timerView.leadingAnchor),
+            progressView.trailingAnchor.constraint(equalTo: timerView.trailingAnchor),
+            
+            softButton.topAnchor.constraint(equalTo: softImageView.topAnchor),
+            softButton.bottomAnchor.constraint(equalTo: softImageView.bottomAnchor),
+            softButton.leadingAnchor.constraint(equalTo: softImageView.leadingAnchor),
+            softButton.trailingAnchor.constraint(equalTo: softImageView.trailingAnchor),
+            
+            mediumButton.topAnchor.constraint(equalTo: mediumImageView.topAnchor),
+            mediumButton.bottomAnchor.constraint(equalTo: mediumImageView.bottomAnchor),
+            mediumButton.leadingAnchor.constraint(equalTo: mediumImageView.leadingAnchor),
+            mediumButton.trailingAnchor.constraint(equalTo: mediumImageView.trailingAnchor),
+            
+            hardButton.topAnchor.constraint(equalTo: hardImageView.topAnchor),
+            hardButton.bottomAnchor.constraint(equalTo: hardImageView.bottomAnchor),
+            hardButton.leadingAnchor.constraint(equalTo: hardImageView.leadingAnchor),
+            hardButton.trailingAnchor.constraint(equalTo: hardImageView.trailingAnchor)
+        ])
+    }
+    
+}

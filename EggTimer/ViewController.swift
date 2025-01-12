f90b2409d6691e5eaefe6df8a68bdb1768f24200
IgorKlevzhits <klevzhits94@icloud.com>
2025-01-12 22:50:19 +0300

    Add logic for eggTimer app

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
@@ -55,10 +56,20 @@ class ViewController: UIViewController {
     
     private lazy var progressView: UIProgressView = {
         let element = UIProgressView()
-        
+        element.progressTintColor = .systemYellow
+        element.trackTintColor = .systemGray
         element.translatesAutoresizingMaskIntoConstraints = false
         return element
     }()
+    
+    // MARK: - Private Properties
+    
+    private let eggTimes = ["Soft": 3, "Medium": 420, "Hard": 720]
+    private var totalTime = 0
+    private var secondPassed = 0
+    private var timer = Timer()
+    private var player: AVAudioPlayer?
+    private var nameSoundTimer = "alarm_sound"
     
     // MARK: - Life Cycle
 
@@ -71,8 +82,39 @@ class ViewController: UIViewController {
     
     // MARK: - Buisness Logic
     
+    private func playSound(_ soundName: String) {
+        guard let url = Bundle.main.url(forResource: soundName, withExtension: "mp3") else { return }
+        
+        player = try! AVAudioPlayer(contentsOf: url)
+        player?.play()
+    }
+    
     @objc private func eggsButtonsTapped(_ sender: UIButton) {
-        print(sender.currentTitle)
+        timer.invalidate()
+        progressView.setProgress(0, animated: true)
+        secondPassed = 0
+        
+        let hardness = sender.titleLabel?.text ?? "error"
+        
+        titleLabel.text = "Your should \(hardness)"
+        
+        totalTime = eggTimes[hardness] ?? 0
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc private func updateTimer() {
+        if secondPassed < totalTime {
+            secondPassed += 1
+            let percentageProgress = Float(secondPassed) / Float(totalTime)
+            progressView.setProgress(percentageProgress, animated: true)
+        } else {
+            playSound(nameSoundTimer)
+            timer.invalidate()
+            secondPassed = 0
+            titleLabel.text = "That's done! Let's go repeats?"
+            progressView.setProgress(1, animated: true)
+        }
     }
 
 }
